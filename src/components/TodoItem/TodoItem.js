ba342d097b2e160fdbedf974cd92f10a2a1e23ce
Kelvin <kelvinpella@gmail.com>
2021-08-12 07:35:29 +0300

    applied few styles to todo containers

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { BiEditAlt } from "react-icons/bi";
 import { AiOutlineDelete } from "react-icons/ai";
+import styles from "./TodoItem.module.css";
 
 const TodoItem = (props) => {
   // conditional rendering of todo string and updateTodo component
   let renderedContent;
   if (typeof props.todo === "string") {
     renderedContent = (
-      <div>
+      <div className={styles.TodoItemContainer}>
         <p>{props.todo}</p>
-        <div onClick={props.updateTodo}>
-          <BiEditAlt />
-        </div>
-        <div onClick={props.deleteTodo}>
-          <AiOutlineDelete />
+        <div>
+          <div onClick={props.updateTodo}>
+            <BiEditAlt />
+          </div>
+          <div onClick={props.deleteTodo}>
+            <AiOutlineDelete />
+          </div>
         </div>
       </div>
     );
